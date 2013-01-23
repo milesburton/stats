be20876df6b6cda49444ffb70a3da19bba023e8b
miles <miles@mnetcs.com>
2013-01-23 22:04:36 +0000

    @Miles added functionals for listing users for a team

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -5,12 +5,15 @@ class UrlMappings {
 
         "/v1/teams"(controller: 'Teams', action: 'list')
         "/v1/teams/search/$q"(controller: 'Teams', action: 'search')
-        "/v1/teams/$teamId/users/search/$q" (controller: 'Users', action: 'searchWithinTeam')
+
         "/v1/teams/$teamId/users/$alias/history" (controller: 'UserHistory', action: 'listBetween')
+
         "/v1/teams/$teamId/history" (controller: 'TeamHistory', action: 'listBetween')
 
         "/v1/users" (controller: 'Users', action: 'list')
         "/v1/users/search/$q" (controller: 'Users', action: 'search')
+        "/v1/teams/$teamId/users" (controller: 'Users', action: 'listForTeam')
+        "/v1/teams/$teamId/users/search/$q" (controller: 'Users', action: 'searchWithinTeam')
 
         "/"(view: "/index")
         "500"(view: '/error')
---
+++ test/functional/com/mb/stats/features/ListUsersForTeamSpec.groovy
@@ -0,0 +1,118 @@
+package com.mb.stats.features
+
+import com.mb.stats.features.base.RemoteServiceGebSpec
+import com.mb.stats.features.fixture.UserFixtures
+import com.popcornteam.restclient.response.RestResponse
+import grails.converters.JSON
+import spock.lang.Unroll
+
+@Mixin(UserFixtures)
+class ListUsersForTeamSpec extends RemoteServiceGebSpec {
+
+    def "list for team"() {
+
+        given:
+        def fixtures = customTeamFixturesForTeam(62, 10)
+
+        when:
+        RestResponse r = jsonClient.get('v1/teams/62/users')
+
+        then:
+        def j = JSON.parse r.bodyAsString
+        j.total == 10
+        j.results == fixtures
+    }
+
+    @Unroll
+    def "list sort by #sort #order"() {
+
+        given:
+        def fixtures = customTeamFixturesForTeam(62, 10)
+
+        and:
+        fixtures.sort { it.hasProperty(sort) ? it."${sort}" : it.ptsTotal }
+        order != "asc" ? fixtures.reverse(true) : ''
+
+        when:
+        RestResponse r = jsonClient.get("v1/teams/62/users?sort=$sort&order=$order")
+
+        then:
+        def j = JSON.parse r.bodyAsString
+        j.total == 10
+        j.results == fixtures[0..9]
+
+        where:
+        sort        | order
+        "ptsTotal"  | "desc"
+        "ptsDelta"  | "desc"
+        "wuTotal"   | "desc"
+        "wuDelta"   | "desc"
+        "rank"      | "desc"
+        "rankDelta" | "desc"
+        "ptsDay"    | "desc"
+        "ptsWeek"   | "desc"
+        "garbage"   | "desc"
+        "ptsDay"    | "garbage"
+        "ptsTotal"  | "asc"
+        "ptsDelta"  | "asc"
+        "wuTotal"   | "asc"
+        "wuDelta"   | "asc"
+        "rank"      | "asc"
+        "rankDelta" | "asc"
+        "ptsDay"    | "asc"
+        "ptsWeek"   | "asc"
+        "garbage"   | "asc"
+        "ptsweek"   | "asc"
+
+    }
+
+    @Unroll
+    def "list illegal offset #offset and limit #limit"() {
+
+        given:
+        def fixtures = customTeamFixturesForTeam(62, 1010)
+
+        when:
+        RestResponse r = jsonClient.get("v1/teams/62/users?limit=$limit&offset=$offset")
+
+        then:
+        def j = JSON.parse r.bodyAsString
+        j.total == 1010
+        j.results == fixtures[expectedOffset..(expectedLimit - 1)]
+
+        where:
+        offset | expectedOffset | limit | expectedLimit
+        -100   | 0              | -1    | 1
+        -1     | 0              | -100  | 1
+        0      | 0              | 10000 | 1000
+        0      | 0              | 1001  | 1000
+    }
+
+    @Unroll
+    def "list offset #offset and limit #limit"() {
+
+        given:
+        def fixtures = customTeamFixturesForTeam(62, 100)
+
+        when:
+        RestResponse r = jsonClient.get("v1/teams/62/users?limit=$limit&offset=$offset")
+
+        then:
+        def j = JSON.parse r.bodyAsString
+        j.total == 100
+        j.results == fixtures[offset..offset + (limit - 1)]
+
+        where:
+        offset | limit
+        0      | 50
+        1      | 50
+        10     | 50
+        50     | 50
+        0      | 100
+        0      | 10
+        0      | 9
+        1      | 9
+        9      | 9
+        50     | 10
+    }
+}
