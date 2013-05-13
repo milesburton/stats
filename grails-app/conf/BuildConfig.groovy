3e5c86da3f76214600b82e55e806c403f98a0eb8
miles <miles@yosemite.(none)>
2013-05-13 06:14:25 +0100

    @Miles cleaned up config, upgraded grails

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,65 +1,53 @@
-grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "3.0" 
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
+    
     inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-        // excludes 'ehcache'
+        
+        
     }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    checksums true // Whether to verify checksums on resolve
+    log "error" 
+    checksums true 
 
-    def gebVersion = "0.9.0-SNAPSHOT"
+    def gebVersion = "0.9.0"
     def seleniumVersion = "2.21.0"
 
     repositories {
-        inherits true // Whether to inherit repository definitions from plugins
+        inherits true 
 
         grailsPlugins()
         grailsHome()
         grailsCentral()
 
-        // mavenLocal()
+        
         mavenCentral()
 
         mavenRepo 'http://artifactory.milesburton.com/artifactory/remote-repos'
         mavenRepo 'http://artifactory.milesburton.com/artifactory/public'
-
         mavenRepo 'http://oss.sonatype.org/content/repositories/snapshots/'
 
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         runtime 'postgresql:postgresql:8.4-702.jdbc3',
                 'com.google.code.gson:gson:2.2.2'
 
-        //  compile ":newrelic:0.5"
         compile 'joda-time:joda-time:2.0'
 
 
         test("org.seleniumhq.selenium:selenium-htmlunit-driver:$seleniumVersion") {
             exclude "xml-apis"
         }
-        test("org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion")
-        test("org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion")
-
-
-        test "org.spockframework:spock-grails-support:0.7-groovy-2.0",
-                "org.gebish:geb-spock:$gebVersion"
-
-        test('com.popcornteam:restclient:1.0.130113.1729')
+        test "org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion",
+             "org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion",
+             "org.spockframework:spock-grails-support:0.7-groovy-2.0",
+             "org.gebish:geb-spock:$gebVersion",
+             'com.popcornteam:restclient:1.0.130113.1729'
 
     }
 
@@ -69,10 +57,6 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.1.6"
         runtime ":cors:1.0.3"
 
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.4"
 
         build ":tomcat:$grailsVersion"
 
